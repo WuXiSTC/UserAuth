17cea379e70e3889182155d2aac47c6d52f37a24
yindaheng98 <yindaheng98@163.com>
2019-09-25 14:20:17 +0800

    Canssandra上的查询用户已完成

--- Dao/QueryUser.go
+++ Dao/QueryUser.go
@@ -6,25 +6,25 @@ import (
 	"./Database"
 )
 
-//验证用户：验证用户名密码是否正确
+//验证用户：验证用户名密码是否正确，如果发现验证的用户不在缓存就会去数据库查找，如果查到了就写缓存
 //
-//如果发现验证的用户不在缓存就会去数据库查找，如果查到了就写缓存
-//
-//返回值是(bool,error)分别表示验证是否通过和错误信息
+//如果验证通过则返回true,nil；用户名密码错误则返回false,nil；出错则返回false,[错误信息]
 func QueryUser(user User) (bool, error) {
+	ok, err := VerifyCachedUser(user) //先查缓存
+	if err == nil {
+		return ok, nil //缓存中有则返回
+	}
+	util.LogE(err)
+
 	ID := user.GetID()
 	PASS := user.GetPASS()
-
-	ok, err := VerifyCachedUser(user) //先查缓存
-	if err == nil && ok {
-		return true, nil //缓存中有则返回
-	}
-
 	cachedPASS, err := Database.GetUserPASS(ID) //否则查数据库
-	util.LogE(err)
-	if err == nil && cachedPASS != "" { //数据库查到了就写入缓存
-		_, errR := Cache.SetUser(ID, PASS)
-		util.LogE(errR)
+	if err != nil {
+		return false, err
 	}
-	return cachedPASS == PASS, err //最后返回
+	if cachedPASS != "" { //数据库查到了就写入缓存
+		_, err := Cache.SetUser(ID, cachedPASS)
+		util.LogE(err)
+	}
+	return cachedPASS != "" && cachedPASS == PASS, nil //最后返回
 }
--- Dao/VerifyCachedUser.go
+++ Dao/VerifyCachedUser.go
@@ -1,20 +1,16 @@
 package Dao
 
 import (
-	"../util"
 	"./Cache"
 )
 
 //仅在缓存中验证用户名和密码
 //
-//验证成功返回(true,nil)
-//用户名密码错误返回(false,nil)
-//其他情况返回(false,[错误信息])
+//验证成功返回(true,nil)；用户名密码错误返回(false,nil)；其他情况返回(false,[错误信息])
 func VerifyCachedUser(user User) (bool, error) {
 	cachedPASS, err := Cache.GetUserPASS(user.GetID())
-	util.LogE(err)
-	if err != nil {
+	if err != nil || cachedPASS == "" {
 		return false, err
 	}
-	return cachedPASS == user.GetPASS(), err
+	return cachedPASS == user.GetPASS(), nil
 }
