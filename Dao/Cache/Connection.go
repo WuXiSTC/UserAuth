99750061c7f4f6ebc08afe2dffd3d10344170837
yindaheng98 <yindaheng98@163.com>
2019-09-26 20:45:30 +0800

    统一了一下Cache和Database的设置类的名字

--- Dao/Cache/Connection.go
+++ Dao/Cache/Connection.go
@@ -5,7 +5,7 @@ import (
 	"github.com/garyburd/redigo/redis"
 )
 
-type config struct {
+type Config struct {
 	ExistTime     uint64 `yaml:"ExistTime"`     //设置每个缓存的存活时间，单位秒
 	RandExistTime int64  `yaml:"RandExistTime"` //设置缓存的随机变化范围，单位秒。防止缓存雪崩
 	InitialCache  uint64 `yaml:"InitialCache"`  //设置初始缓存数量，在服务器启动时写入多少记录，单位条
@@ -14,7 +14,7 @@ type config struct {
 }
 
 var db redis.Conn = nil
-var Conf = config{6000, 300, 300, "tcp", "127.0.0.1:6379"}
+var Conf = Config{6000, 300, 300, "tcp", "127.0.0.1:6379"}
 
 func ConfigureRedis(path string) {
 	util.GetConf(path, &Conf)
